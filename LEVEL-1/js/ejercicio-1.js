1b3040a46b9a84f5d40246c652624c47793c604c
Peter Aspiazu <paspiazusabando@gmail.com>
2022-09-14 16:56:47 -0500

    Resolviendo ejercicios de codewars - resumiendo primer ejercicio

--- LEVEL-1/js/ejercicio-1.js
+++ LEVEL-1/js/ejercicio-1.js
@@ -2,24 +2,34 @@
 Complete la solución para que devuelva true si el primer argumento (array) pasado termina con el segundo argumento (también un array).
 */ 
 
-
+// SOLUCION RECOMENDADA
 function solution(str, ending){
+    return str.endsWith(ending);
+}
+
+// ESTA SOLUCION ES DE ALGUIEN QUE NO CONOCE BIEN JAVASCRIPT
+/*function solution(str, ending){
     // TODO: complete
-    const lastCharacter1 = str.split("")[str.length - 1]
-    const lastCharacter2 = ending.split("")[ending.length - 1]
     
-    if(str != '' && ending != ''){
-      if(str.includes(ending) && lastCharacter1 === lastCharacter2 ){
+    const arr1 = str.split('')
+    const arr2 = arr1.slice(-ending.length)
+    const arr3 = arr2.join('')
+    
+    if(str !== '' && ending !== ''){
+      
+      if(arr3 === ending){
         console.log(true)
         return true
-      }else {
+      } else {
         console.log(false)
         return false
       }
+      
     } else {
       return true
     }
-}
+    
+  }*/
 
-solution('abc', 'bc') // returns true
-solution('abc', 'd') // returns false
+console.log( solution('abc', 'bc') ) // returns true
+console.log( solution('abc', 'd') ) // returns false
