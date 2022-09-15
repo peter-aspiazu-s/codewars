df3b54cbdde4d999219ddb2f810662ebca1fd42b
Peter Aspiazu <paspiazusabando@gmail.com>
2022-09-15 16:49:42 -0500

    Actualizando ejercicio 2 con dos soluciones mejores soluciones que las mías

--- LEVEL-1/js/ejercicio-2.js
+++ LEVEL-1/js/ejercicio-2.js
@@ -19,7 +19,17 @@
   }
 
   createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
-
+  
+  // SOLUCION ALTAMENTE RECOMENDADA
+//   function createPhoneNumber(numbers){
+//       numbers = numbers.join('');
+//       return '(' + numbers.substring(0, 3) + ') ' 
+//       + numbers.substring(3, 6) 
+//       + '-' 
+//       + numbers.substring(6);
+//     }
+    
+//     createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 
 // ESTA SOLUCION NO ES RECOMENDADA
 
