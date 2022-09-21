53d5b277d0311e1561da5150b0a278bc85818688
Peter Aspiazu <paspiazusabando@gmail.com>
2022-09-21 11:10:09 -0500

    subiendo ejercicio #5 no es mi solución

--- LEVEL-1/js/ejercicio-4.js
+++ LEVEL-1/js/ejercicio-4.js
@@ -26,7 +26,6 @@ function alphabetPosition(text) {
 /*let abc = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
-
 const alphabetPosition = (text) => {
 
     const arrText = text.toLowerCase().split('')
---
+++ LEVEL-1/js/ejercicio-5.js
@@ -0,0 +1,20 @@
+/**
+ * Se le dará un número y deberá devolverlo como una cadena en forma expandida. Por ejemplo:
+ * expandedForm(12); // Should return '10 + 2'
+ * expandedForm(42); // Should return '40 + 2'
+ * expandedForm(70304); // Should return '70000 + 300 + 4'
+ */
+
+
+// ESTA SOLUCIÓN NO ES MÍA NO LOGRÉ COMPLETAR ESTE EJERCICIO
+ const expandedForm = (num) => {
+
+    // primero convierte el numero a string y luego a un arreglo, después usa el map para acceder al valor, al indice y al arreglo, luego retorna el valor concatenando el 0 en string y como es string lo puede repetir
+    // pero lo repite por la longitud del array menos el indice y menos 1 para que de justo, luego ese array lo filtra y busca el valor entero que sean diferente de 0
+    // y ya por último lo convierten a string pero separado por un + que cabron el que lo hizo.
+    
+   return num.toString().split('').map((el,index,array)=>{return el + "0".repeat(array.length - index -1)}).filter(el=>parseInt(el)!=0).join(" + ");
+
+  }
+
+  console.log(expandedForm(70304))
