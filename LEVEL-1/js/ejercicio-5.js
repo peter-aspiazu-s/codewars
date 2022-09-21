55829ca571f0fc7410227ae0e5024407d968936a
Peter Aspiazu <paspiazusabando@gmail.com>
2022-09-21 11:18:11 -0500

    agregando mas soluciones del ejercicio 5 no son mias

--- LEVEL-1/js/ejercicio-5.js
+++ LEVEL-1/js/ejercicio-5.js
@@ -17,4 +17,13 @@
 
   }
 
+  // SOLUCIONES DE OTRAS PERSONAS
+  /*const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");*/
+
   console.log(expandedForm(70304))
